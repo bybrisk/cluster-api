7de517e83c28ce57a423be903455dcdc6729c1c7
bybrisk <imctobybrisk@gmail.com>
2021-02-24 18:39:42 +0530

    permissible move modified

--- data/modifiedKMean.go
+++ data/modifiedKMean.go
@@ -3,7 +3,8 @@ package data
 import (
 	"fmt"
 	"math"
-	"github.com/segmentio/ksuid")
+	"github.com/segmentio/ksuid"
+	"sort")
 
 var MKM_C []int64
 type MKM_intArr []int64
@@ -18,6 +19,11 @@ var MKM_Weights_Sum float64
 type MKM_floatArr []float64
 var MKM_LatLong []float64
 var MKM_DataPoints []MKM_floatArr
+
+type kv struct {
+	Key   int64
+	Value float64
+}
 
 
 func ModifiedCluster (geoCodeArr []LatLongAndID,k int) ([]string, []string, []MKM_intArr, []MKM_floatArr ) {
@@ -186,6 +192,19 @@ func FindCentriodVal(MKM_OmegaNew []MKM_intArr) []MKM_floatArr{
 	return MKM_CentroidArray
 }
 
+func FindCentriodSingle(singleOmega []int64) []float64{
+
+	cc := make([]float64, 2)
+    for _,v:= range singleOmega{
+		cc[0] = cc[0] + MKM_DataPoints[v][0]
+		cc[1] = cc[1] + MKM_DataPoints[v][1]
+	}
+	cc[0] = cc[0]/float64(len(singleOmega))
+	cc[1] = cc[1]/float64(len(singleOmega))
+
+	return cc
+}
+
 func getDistance(lat1 float64,long1 float64,lat2 float64,long2 float64) float64{
 	var r float64
 	r = math.Pow(lat1-lat2, 2) + math.Pow(long1-long2, 2)
@@ -249,7 +268,7 @@ func removeItem(items []int64, item int) []int64 {
     return newitems
 }
 
-func AdmissibleMoveImproved(oNew *[]MKM_intArr, k int,Navg int) *[]MKM_intArr {
+/*func AdmissibleMoveImproved(oNew *[]MKM_intArr, k int,Navg int) *[]MKM_intArr {
 	var counter int64
 
 	for i:=0;i<len(*oNew);i++{
@@ -259,6 +278,73 @@ func AdmissibleMoveImproved(oNew *[]MKM_intArr, k int,Navg int) *[]MKM_intArr {
 			if j!=i {
 
 				CrPi := (*oNew)[i]
+				//iterate over Cr
+				for l:=0;l<len((*oNew)[i]);l++{
+					//fmt.Printf("Value in Cr [%d] %d\n",k,(*oNew)[i][l])
+					//fmt.Printf("Value in Cr [%d] %d\n",k,CrPi)
+					valShifted := (*oNew)[i][l]
+					CrPi=removeItem((*oNew)[i],int(valShifted))
+					//fmt.Printf("After Deleting ------------->%v\n",CrPi)
+					var CsPi []int64
+
+					//iterate through Cs
+					for m:=0;m<len((*oNew)[j]);m++{
+						CsPi=append((*oNew)[j],valShifted)	
+						//fmt.Printf("After Adding ------------->%v\n",CsPi)
+
+						//Balencing condition
+						if (balenceValue((*oNew)[i],(*oNew)[j], Navg) > 0 || (balenceValue((*oNew)[i],(*oNew)[j], Navg)>0 || balenceValue((*oNew)[i],(*oNew)[j], Navg)==0 && weightValue((*oNew)[i],(*oNew)[j],CrPi,CsPi,k)>0)) {
+							counter = counter+1
+							(*oNew)[i]=CrPi
+							(*oNew)[j]=CsPi
+							break
+						}
+						CsPi=nil
+					}
+
+				}
+			}
+		}
+	}
+	return oNew
+}*/
+
+func AdmissibleMoveImproved(oNew *[]MKM_intArr, k int,Navg int) *[]MKM_intArr {
+	var counter int64
+
+	for i:=0;i<len(*oNew);i++{
+		//fmt.Printf("Value of ONew After one cluster [%d] %v\n",i,*oNew)
+		
+		for j:=0;j<len(*oNew);j++{
+			if j!=i {
+
+				// centroid of (*oNew)[j]
+				ojCentroid:=FindCentriodSingle((*oNew)[j])
+				//fmt.Printf("centroid [%d] : %v",i+j,ojCentroid)
+
+				//sort (*oNew)[i] on the basis of distance from centroid
+				distanceFromCentroidMap := make(map[int64]float64)
+				for fIndex:=0;fIndex<len((*oNew)[i]);fIndex++{
+					distanceFromCentroid := getDistance(MKM_DataPoints[(*oNew)[i][fIndex]][0],MKM_DataPoints[(*oNew)[i][fIndex]][1],ojCentroid[0],ojCentroid[1])
+					distanceFromCentroidMap[(*oNew)[i][fIndex]] = distanceFromCentroid
+				}
+				//fmt.Printf("distanceFromCentroidMap [%d] : %v",i+j,distanceFromCentroidMap)
+				//sorting here
+				var sortedSlicePoint []kv
+				for indexPointOfMap, distanceValueOfMap := range distanceFromCentroidMap {
+					sortedSlicePoint = append(sortedSlicePoint, kv{indexPointOfMap, distanceValueOfMap})
+				}
+				sort.Slice(sortedSlicePoint, func(x, y int) bool {
+					return sortedSlicePoint[x].Value < sortedSlicePoint[y].Value
+				})
+				for indexOfOriginalArraySlected, indexPointOfMapInPlace := range sortedSlicePoint {
+					(*oNew)[i][indexOfOriginalArraySlected]=indexPointOfMapInPlace.Key
+				}
+				sortedSlicePoint = nil
+				//fmt.Printf("Sorted array [%d] : %v",i+j,(*oNew)[i])
+
+				CrPi := (*oNew)[i]
+
 				//iterate over Cr
 				for l:=0;l<len((*oNew)[i]);l++{
 					//fmt.Printf("Value in Cr [%d] %d\n",k,(*oNew)[i][l])
--- handlers/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/bybrisk/cluster-api/handlers
-
-go 1.15
-
-require (
-	github.com/bybrisk/cluster-api/data v0.0.0-20210223095235-e1d82a7e5a15
-	github.com/gorilla/mux v1.8.0
-)
